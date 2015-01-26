08f0201e0771743549f8e3a387e380380ba50e11
Astn <austin.w.harris@gmail.com>
2015-01-26 00:14:33 -0700

    Just starting

---
+++ ConsoleHost/ConsoleHost/Env.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleHost
+{
+    public class Env
+    {
+        public BlockType Type { get; set; }
+
+    }
+
+    public enum BlockType
+    {
+        Empty=0,
+        Solid=1;
+        Water=2;
+
+    }
+}
